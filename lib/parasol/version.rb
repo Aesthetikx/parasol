6b83b988ca44cdb934800335feff40271b75f5da
John DeSilva <john@aesthetikx.info>
2024-01-13 19:26:38 -0500

    Genesis commit

---
+++ lib/parasol/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Parasol
+  VERSION = "0.1.0"
+end
---
+++ spec/parasol_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Parasol do
+  it "has a version number" do
+    expect(Parasol::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
