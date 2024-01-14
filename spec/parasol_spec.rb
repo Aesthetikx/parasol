1ac99abd0d5880920fe5def357ca0063ca05b56f
John DeSilva <john@aesthetikx.info>
2024-01-14 09:06:04 -0500

    Remove empty spec

--- spec/parasol_spec.rb
+++ spec/parasol_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Parasol do
   it "has a version number" do
     expect(Parasol::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
